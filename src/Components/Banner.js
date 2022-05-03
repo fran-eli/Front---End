bd0825e9f8a6b9a9af8e4a00d50d383651df90f7
franciscoluna-28 <franciscoluna2004@gmail.com>
2022-05-03 16:27:28 -0400

    init

---
+++ src/Components/Banner.js
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return(
+        <h1>
+            This is a Banner
+        </h1>
+    )
+}
+
+export default Banner
+
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <h1>
+            lmao
+        </h1>
+    )
+}
+
+export default Header
+
